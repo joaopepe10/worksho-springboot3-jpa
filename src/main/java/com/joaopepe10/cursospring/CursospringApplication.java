00f2203dbc319b330649a48e2be4ddb01b33c503
João Perdro Pires <joaopedropires61@gmail.com>
2023-05-24 12:18:33 -0300

    Criando projeto

---
+++ src/main/java/com/joaopepe10/cursospring/CursospringApplication.java
@@ -0,0 +1,17 @@
+package com.joaopepe10.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+import java.util.zip.DataFormatException;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CursospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+}
